af8c00ba1f13807ac740029c710684d3d42d0da2
Adam H. Sparks <adamhsparks@icloud.com>
2023-09-03 18:54:30 +0800

    Improve tests by splitting them into individual tests rather than a mono-test

--- tests/testthat/test-autoplot_hagis_complexities.R
+++ tests/testthat/test-autoplot_hagis_complexities.R
@@ -1,29 +1,35 @@
 
 # test plot.hagis.gene.summary ----------------------------------------------
-test_that("auto.plot.hagis.complexities() returns a ggplot2 object", {
-  data(P_sojae_survey)
-  complexities <- calculate_complexities(
-    x = P_sojae_survey,
-    cutoff = 60,
-    control = "susceptible",
-    sample = "Isolate",
-    gene = "Rps",
-    perc_susc = "perc.susc"
-  )
+data(P_sojae_survey)
+complexities <- calculate_complexities(
+  x = P_sojae_survey,
+  cutoff = 60,
+  control = "susceptible",
+  sample = "Isolate",
+  gene = "Rps",
+  perc_susc = "perc.susc"
+)
+
+test_that("auto.plot.hagis.complexities() returns a ggplot2 object 4 cnt", {
   complexities_count <- autoplot(complexities, type = "count")
   vdiffr::expect_doppelganger("count_complexities",
                       complexities_count)
+})
 
+test_that("auto.plot.hagis.complexities() returns a ggplot2 object 4 %", {
   complexities_perc <- autoplot(complexities, type = "percentage")
   vdiffr::expect_doppelganger("perc_complexities",
                       complexities_perc)
-
+})
+test_that("auto.plot.hagis.complexities() rtns a blue ggplot2 object 4 %", {
   complexities_perc_blue <-
     autoplot(complexities, type = "percentage",
              color = "blue")
   vdiffr::expect_doppelganger("perc_complexities_blue",
                       complexities_perc_blue)
+})
 
+test_that("auto.plot.hagis.complexities() rtns descending ggplot2 object 4 %", {
   complexities_perc_desc_blue <-
     autoplot(complexities,
              type = "percentage",
@@ -31,7 +37,9 @@ test_that("auto.plot.hagis.complexities() returns a ggplot2 object", {
              order = "descending")
   vdiffr::expect_doppelganger("perc_complexities_desc_blue",
                       complexities_perc_desc_blue)
+})
 
+test_that("auto.plot.hagis.complexities() rtns ascending ggplot2 object 4 %", {
   complexities_perc_asc_blue <-
     autoplot(complexities,
              type = "percentage",
@@ -39,7 +47,9 @@ test_that("auto.plot.hagis.complexities() returns a ggplot2 object", {
              order = "ascending")
   vdiffr::expect_doppelganger("perc_complexities_asc_blue",
                       complexities_perc_asc_blue)
+})
 
+test_that("auto.plot.hagis.complexities() rtns ascend ggplot2 object 4 cnt", {
   complexities_count_asc_blue <-
     autoplot(complexities,
              type = "count",
@@ -47,7 +57,9 @@ test_that("auto.plot.hagis.complexities() returns a ggplot2 object", {
              order = "ascending")
   vdiffr::expect_doppelganger("count_complexities_asc_blue",
                       complexities_count_asc_blue)
+})
 
+test_that("auto.plot.hagis.complexities() errors on invalid type", {
   expect_error(autoplot(complexities, type = "orange"),
                regexp = "You have entered an invalid `type`.")
 })
--- tests/testthat/test-individual_pathotypes.R
+++ tests/testthat/test-individual_pathotypes.R
@@ -1,22 +1,24 @@
 
 # test diversities table -------------------------------------------------------
 data(P_sojae_survey)
-diversities <- calculate_diversities(x = P_sojae_survey,
-                                     cutoff = 60,
-                                     control = "susceptible",
-                                     sample = "Isolate",
-                                     gene = "Rps",
-                                     perc_susc = "perc.susc")
+diversities <- calculate_diversities(
+  x = P_sojae_survey,
+  cutoff = 60,
+  control = "susceptible",
+  sample = "Isolate",
+  gene = "Rps",
+  perc_susc = "perc.susc"
+)
 
-test_that("individual_pathotypes() returns a pander object",
-          {expect_type(
-            capture.output(
-              individual_pathotypes(x = diversities, type = "text")),
+test_that("individual_pathotypes() returns a pander object", {
+            expect_type(
+              capture.output(
+                individual_pathotypes(x = diversities, type = "text")),
                         "character")
           })
 
 test_that("individual_pathotypes() stops if object is not hagis.diversities
-          object",
-          {expect_error(individual_pathotypes("y"),
-                        regexp = "This is not a hagis.diversities object.")
+          object", {
+            expect_error(individual_pathotypes("y"),
+                         regexp = "This is not a hagis.diversities object.")
           })
