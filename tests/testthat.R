211694611e7d01e9604eb27f1a38fd790bb5c000
Adam H. Sparks <adamhsparks@icloud.com>
2023-09-06 07:53:32 +0800

    Only run any and all tests if environment is NOT CRAN due to CRAN being CRAN

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,12 @@
-library(testthat)
-library(vdiffr)
-library(hagis)
+library("testthat")
+library("vdiffr")
+library("hagis")
 
-test_check("hagis")
+
+if (Sys.getenv("NOT_CRAN") == "true") {
+  # like global skip_on_cran
+  # https://github.com/r-lib/testthat/issues/144#issuecomment-396902791
+  # according to https://github.com/hadley/testthat/issues/144
+  Sys.setenv("R_TESTS" = "")
+  test_check("hagis")
+}
